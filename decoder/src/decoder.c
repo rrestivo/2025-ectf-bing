83feff318d89cba377a3b78a40a1dcd96536ca7a
lwmurph <lwmurphy02@gmail.com>
2025-02-18 15:16:31 -0500

    small changes to debug statements

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -279,42 +279,7 @@ int update_subscription(pkt_len_t pkt_len, subscription_update_packet_t *update)
     return 0;
 }
 /*************  https://stackoverflow.com/questions/3408706/hexadecimal-string-to-byte-array-in-c  *************** */
-uint8_t* hexToByteArray(char* string) {
-
-    if(string == NULL) 
-       return NULL;
-
-    size_t slength = strlen(string);
-    if((slength % 2) != 0) // must be even
-       return NULL;
-
-    size_t dlength = slength / 2;
-
-    uint8_t* data = malloc(dlength);
-    memset(data, 0, dlength);
-
-    size_t index = 0;
-    while (index < slength) {
-        char c = string[index];
-        int value = 0;
-        if(c >= '0' && c <= '9')
-          value = (c - '0');
-        else if (c >= 'A' && c <= 'F') 
-          value = (10 + (c - 'A'));
-        else if (c >= 'a' && c <= 'f')
-          value = (10 + (c - 'a'));
-        else {
-          free(data);
-          return NULL;
-        }
-
-        data[(index/2)] += value << (((index + 1) % 2) * 4);
-
-        index++;
-    }
-
-    return data;
-}
+//hex to byte array logic
 /****************************************************************************************************************** */
 /**
  * @brief Writes the secret key to flash memory on first boot.
